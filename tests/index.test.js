ed1ca1a3bfc1d87a0b4b957bcf1d27dfba715007
Jamie Connolly <jamie.connolly@tailify.com>
2019-01-07 15:23:33 +0000

    Add in a basic Jest test

---
+++ tests/index.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const stylelint = require('stylelint');
+
+it('should not contain invalid rules', async () => {
+  const { errored, results } = await stylelint.lint({
+    code: 'a { font-weight: 500; }\n',
+    codeFilename: 'valid.css',
+    configFile: './lib/index.js',
+  });
+  const { warnings} = results[0];
+
+  expect(errored).toBeFalsy();
+  expect(warnings.length).toBe(0);
+});
