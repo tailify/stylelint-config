b0b2f3392bf6e9f01cdadddfecec1c75dd5b2f3d
Jamie Connolly <jamie.connolly@tailify.com>
2019-01-11 17:28:33 +0000

    Add in some rules to limit the language features

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,7 +4,7 @@ const stylelint = require('stylelint');
 
 it('should not contain invalid rules', async () => {
   const { errored, results } = await stylelint.lint({
-    code: 'a {\n  font-weight: 500;\n}\n',
+    code: '.button {\n  font-weight: 500;\n}\n',
     codeFilename: 'valid.css',
     configFile: './lib/index.js',
   });
